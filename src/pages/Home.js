4fad97ee6aae1258d1031b30ab337422ad858410
Ing140943 <skladeeing@gmail.com>
2023-01-19 23:27:06 +0700

    Edit async function in home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,16 +10,14 @@ const Home = () => {
 
 
   useEffect(() => {
-    const fetchWorkouts = async () => {
+    (async () => {
       const response = await fetch("/api/workouts")
       const json = await response.json();
 
       if (response.ok) {
         dispatch({ type: "SET_WORKOUTS", payload: json });
       }
-    };
-
-    fetchWorkouts();
+    })();
   }, [dispatch]);
 
   return (
